1f11e35b6fae1834c952ee3e376c2e129ad17c09
LAYSEGABI <laysegabrielly13@gmail.com>
2025-12-02 10:52:55 -0300

    Alterações de segurança 2

--- src/main/java/br/com/scripta_api/usuario_service/UsuarioServiceApplication.java
+++ src/main/java/br/com/scripta_api/usuario_service/UsuarioServiceApplication.java
@@ -3,12 +3,17 @@ package br.com.scripta_api.usuario_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean; // Importante
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; // Importante
+import org.springframework.web.cors.CorsConfiguration; // Importante
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // Importante
+import org.springframework.web.filter.CorsFilter; // Importante
 
-// Removemos o ManagementWeb... e deixamos só o SecurityAutoConfiguration
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootApplication
 @ComponentScan(basePackages = "br.com.scripta_api")
 @EnableJpaRepositories(basePackages = "br.com.scripta_api")
 @EntityScan(basePackages = "br.com.scripta_api")
@@ -18,4 +23,29 @@ public class UsuarioServiceApplication {
         SpringApplication.run(UsuarioServiceApplication.class, args);
     }
 
+    // --- A MARRETADA DO CORS ---
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+
+        config.setAllowCredentials(true);
+
+
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+
+
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"));
+
+
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
+
+        config.setExposedHeaders(Arrays.asList("Authorization", "Link", "X-Total-Count"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
